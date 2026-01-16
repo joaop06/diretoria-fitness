448338327b7ae7e4d9c490fb0f325ad0ff312387
João Pedro <joaoofficialpedro@gmail.com>
2026-01-16 01:43:52 -0300

    Added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [{
+    name: 'diretoria-fitness',
+    script: './server.js',
+    instances: 1,
+    exec_mode: 'fork',
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+    },
+    env_development: {
+      NODE_ENV: 'development',
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    merge_logs: true,
+    autorestart: true,
+    max_restarts: 10,
+    min_uptime: '10s',
+    restart_delay: 4000
+  }]
+};
